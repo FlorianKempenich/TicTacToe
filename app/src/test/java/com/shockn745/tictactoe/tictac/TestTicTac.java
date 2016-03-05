0b379351a29579c8beab27b39ef2fe29afe7918a
Florian Kempenich <ShockN745@gmail.com>
2016-03-05 20:38:25 +0100

    Cleanup

--- app/src/test/java/com/shockn745/tictactoe/tictac/TestTicTac.java
+++ app/src/test/java/com/shockn745/tictactoe/tictac/TestTicTac.java
@@ -1,7 +1,6 @@
 package com.shockn745.tictactoe.tictac;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -19,17 +18,37 @@ public class TestTicTac {
 
     }
 
-    @Test (expected = IllegalMoveException.class)
-    public void playTwoMovesSameSpot_ThrowIllegalMoveExceptionnew() throws Exception {
+    @Test(expected = IllegalMoveException.class)
+    public void playTwoMovesSameSpot_ThrowIllegalMoveException() throws Exception {
         game.play(new Move(0, 0, Move.PLAYER_1));
         game.play(new Move(0, 0, Move.PLAYER_2));
     }
 
     @Test
+    public void samePlayerPlays2Rounds_throwException() throws Exception {
+        game.play(new Move(0, 0, Move.PLAYER_1));
+        try {
+            game.play(new Move(0, 1, Move.PLAYER_1));
+            fail();
+        } catch (IllegalMoveException e) {
+            assertEquals("This player just played", e.getMessage());
+        }
+
+    }
+
+    @Test
     public void threeInALine_samePlayer_gameIsFinished() throws Exception {
         scoreLinePlayerOne(0);
-
         assertTrue("Game should be finished", game.isFinished());
+    }
+
+    private void scoreLinePlayerOne(int lineIndex) throws Exception {
+        int otherLine = lineIndex == 0 ? 1 : 0;
+        game.play(new Move(0, lineIndex, Move.PLAYER_1));
+        game.play(new Move(2, otherLine, Move.PLAYER_2));
+        game.play(new Move(1, lineIndex, Move.PLAYER_1));
+        game.play(new Move(1, otherLine, Move.PLAYER_2));
+        game.play(new Move(2, lineIndex, Move.PLAYER_1));
     }
 
     @Test
@@ -45,26 +64,5 @@ public class TestTicTac {
         game.play(new Move(2, 0, Move.PLAYER_1));
 
         assertFalse("Game should NOT be finished", game.isFinished());
-    }
-
-    @Test
-    public void samePlayerPlays2Rounds_throwException() throws Exception {
-        game.play(new Move(0,0, Move.PLAYER_1));
-        try {
-            game.play(new Move(0,1, Move.PLAYER_1));
-            fail();
-        } catch (IllegalMoveException e) {
-            assertEquals("This player just played", e.getMessage());
-        }
-
-    }
-
-    private void scoreLinePlayerOne(int lineIndex) throws Exception {
-        int otherLine = lineIndex == 0 ? 1 : 0;
-        game.play(new Move(0, lineIndex, Move.PLAYER_1));
-        game.play(new Move(2, otherLine, Move.PLAYER_2));
-        game.play(new Move(1, lineIndex, Move.PLAYER_1));
-        game.play(new Move(1, otherLine, Move.PLAYER_2));
-        game.play(new Move(2, lineIndex, Move.PLAYER_1));
     }
 }
