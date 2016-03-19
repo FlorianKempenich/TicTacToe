c3b4c3669526de9b399771812fb9bedaea3fdcf5
Florian Kempenich <ShockN745@gmail.com>
2016-03-19 13:34:21 +0100

    Implement tic tac with custom view

--- presentation/src/main/java/com/shockn745/presentation/MainActivity.java
+++ presentation/src/main/java/com/shockn745/presentation/MainActivity.java
@@ -17,26 +17,10 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity implements MainContract.View {
+public class MainActivity extends AppCompatActivity implements MainContract.View, TicTacView.OnSquareClickedListener {
 
-    @Bind(R.id.button00)
-    Button button00;
-    @Bind(R.id.button10)
-    Button button10;
-    @Bind(R.id.button20)
-    Button button20;
-    @Bind(R.id.button01)
-    Button button01;
-    @Bind(R.id.button11)
-    Button button11;
-    @Bind(R.id.button21)
-    Button button21;
-    @Bind(R.id.button02)
-    Button button02;
-    @Bind(R.id.button12)
-    Button button12;
-    @Bind(R.id.button22)
-    Button button22;
+    @Bind(R.id.main_tictac_view)
+    TicTacView ticTacView;
 
     private MainContract.Presenter presenter;
 
@@ -45,6 +29,9 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        ticTacView.setListener(this);
+
         initPresenter();
         presenter.onCreate();
     }
@@ -56,59 +43,14 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
         presenter = new MainPresenter(this, initNewGameUseCase, addMoveUseCase);
     }
 
-    @OnClick(R.id.button00)
-    void on00Clicked() {
-        onClicked(0, 0);
-    }
-
-    void onClicked(int x, int y) {
+    @Override
+    public void onSquareClicked(int x, int y) {
         presenter.onSquareClicked(x, y);
-        Snackbar.make(button00, "Button : x=" + x + " y=" + y + " clicked!", Snackbar.LENGTH_SHORT).show();
-    }
-
-    @OnClick(R.id.button10)
-    void on10Clicked() {
-        onClicked(1, 0);
-    }
-
-    @OnClick(R.id.button20)
-    void on20Clicked() {
-        onClicked(2, 0);
-    }
-
-    @OnClick(R.id.button01)
-    void on01Clicked() {
-        onClicked(0, 1);
-    }
-
-    @OnClick(R.id.button11)
-    void on11Clicked() {
-        onClicked(1, 1);
-    }
-
-    @OnClick(R.id.button21)
-    void on21Clicked() {
-        onClicked(2, 1);
-    }
-
-    @OnClick(R.id.button02)
-    void on02Clicked() {
-        onClicked(0, 2);
-    }
-
-    @OnClick(R.id.button12)
-    void on12Clicked() {
-        onClicked(1, 2);
-    }
-
-    @OnClick(R.id.button22)
-    void on22Clicked() {
-        onClicked(2, 2);
+        Snackbar.make(ticTacView, "Button : x=" + x + " y=" + y + " clicked!", Snackbar.LENGTH_SHORT).show();
     }
 
     @Override
     public void setSquareText(String text, int x, int y) {
-        if (x == 0 && y == 0)
-            button00.setText(text);
+        ticTacView.setSquareText(text, x, y);
     }
 }
---
+++ presentation/src/main/java/com/shockn745/presentation/TicTacView.java
@@ -0,0 +1,113 @@
+package com.shockn745.presentation;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+/**
+ * @author Kempenich Florian
+ */
+public class TicTacView extends LinearLayout implements View.OnClickListener {
+
+    Button[][] buttons = new Button[3][3];
+    private OnSquareClickedListener listener;
+
+    public TicTacView(Context context) {
+        this(context, null);
+    }
+
+    public TicTacView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public TicTacView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initViews();
+    }
+
+    private void initViews() {
+        setOrientation(VERTICAL);
+        initButtons();
+        addLine(0);
+        addLine(1);
+        addLine(2);
+    }
+
+    private void initButtons() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                Button button = makeNewSquare();
+                button.setOnClickListener(this);
+                buttons[i][j] = button;
+            }
+        }
+    }
+
+    private Button makeNewSquare() {
+        Button button = new Button(getContext());
+        int id = View.generateViewId();
+        button.setId(id);
+        return button;
+    }
+
+    private void addLine(int lineIndex) {
+        LinearLayout firstLine = new LinearLayout(getContext());
+        for (int i = 0; i < 3; i++) {
+            Button button = buttons[i][lineIndex];
+            firstLine.addView(button, makeSquareLayoutParams());
+        }
+        addView(firstLine, makeLineLayoutParams());
+    }
+
+    private LayoutParams makeSquareLayoutParams() {
+        LayoutParams params = new LayoutParams(0, ViewGroup.LayoutParams.MATCH_PARENT);
+        params.weight = 1;
+        return params;
+    }
+
+    private LayoutParams makeLineLayoutParams() {
+        LayoutParams params = new LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                0
+        );
+        params.weight = 1;
+        return params;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        parseCoordinatesAndRedirect(id);
+    }
+
+    private void parseCoordinatesAndRedirect(int id) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (id == buttons[i][j].getId()) {
+                    onSquareClick(i, j);
+                }
+            }
+        }
+    }
+
+    private void onSquareClick(int x, int y) {
+        if (listener != null) {
+            listener.onSquareClicked(x, y);
+        }
+    }
+
+    public void setListener(OnSquareClickedListener listener) {
+        this.listener = listener;
+    }
+
+    public void setSquareText(String text, int x, int y) {
+        buttons[x][y].setText(text);
+    }
+
+    public interface OnSquareClickedListener {
+        void onSquareClicked(int x, int y);
+    }
+}
